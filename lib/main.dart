d15fd31d50373303c98137f3569e836d6b2d206b
Kurogoma939 <kan.sss.939@gmail.com>
2023-10-01 16:04:02 +0900

    AnimatedFractionallySizedBoxのデモ

--- lib/main.dart
+++ lib/main.dart
@@ -25,34 +25,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final double _sliderValue = 0.1;
+  double _sliderValue = 0.1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('MediaQuery Demo'),
       ),
-      body: Column(
-        children: [
-          Flexible(
-            child: FractionallySizedBox(
-              alignment: Alignment.topCenter,
-              widthFactor: 0.8,
-              heightFactor: 0.1,
-              child: Container(
-                color: Colors.amber,
+      body: SizedBox.expand(
+        child: Column(
+          children: [
+            Flexible(
+              child: AnimatedFractionallySizedBox(
+                alignment: Alignment.topCenter,
+                widthFactor: _sliderValue,
+                heightFactor: 0.2,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                child: Container(
+                  color: Colors.amber,
+                ),
               ),
             ),
-          ),
-          // FractionallySizedBox(
-          //   alignment: Alignment.topCenter,
-          //   widthFactor: 0.8,
-          //   heightFactor: 0.1,
-          //   child: Container(
-          //     color: Colors.amber,
-          //   ),
-          // ),
-        ],
+            const Flexible(
+              child: FractionallySizedBox(
+                heightFactor: 0.1,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (_sliderValue < 1.0) {
+                    _sliderValue += 0.05;
+                  } else {
+                    _sliderValue = 0.1;
+                  }
+                });
+              },
+              child: const Text('Animation'),
+            ),
+          ],
+        ),
       ),
     );
   }
